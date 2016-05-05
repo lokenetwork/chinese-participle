432f9e9dcb82bc9c9d246ecb1ff55abab861173a
loken <lokenetwork@users.noreply.github.com>
2016-05-05 12:22:10 +0800

    change to english

--- main.h
+++ main.h
@@ -3,19 +3,17 @@
 //
 #ifndef PULLWORDS_MAIN_H
 #define PULLWORDS_MAIN_H
-//http包头长度
+//The max lenght of http request
 #define HTTP_REQUEST_LEN 1500
-//分词结果的最大长度
+//The max lenght of participle result
 #define WORD_RESULT_LEN 200
-//可处理的最大的客户端发送的搜索词长度
+//The max lenght of search words in http request
 #define MAX_SEARCH_WORDS_LEN 200
-//最多开多少个线程处理分词
+//The max thread for a http request
 #define MAX_CUT_WORD_THREAD_NUMBER 30
-//各线程分词结果结构体
+//participle result strcut
 struct TREE_CUT_WORDS_RESULT {
-    //分词结果
     char pullwords_result[MAX_SEARCH_WORDS_LEN];
-    //分词权重
     int words_weight;
 };
 #define printd(fmt,args) printf(__FILE__ "(%d): " fmt, __LINE__, ##args);
